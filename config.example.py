908608de284866fe7ab6e46ccfd42d6013d83541
Oksana Shulha <sanka.shulha@gmail.com>
2026-02-23 15:00:59 +0200

    Stop tracking config.py; add config.example.py

---
+++ config.example.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    "dbname": "sales",
+    "user": "postgres",
+    "password": "YOUR_PASSWORD_HERE",
+    "host": "localhost",
+    "port": "5432"
+}
+
+EXPORT_FILE_PATH = r"C:\Sales_App\exports\report.txt"
